bcd6372b608abd98b500ae954d72e9be90f1f263
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-08-29 13:33:25 +0330

    Add "TwoPointer -> Palindrome.cs"

---
+++ Project/Problems/TwoPointers/Palindrome.cs
@@ -0,0 +1,33 @@
+namespace Project.Problems.TwoPointers;
+
+public class Palindrome
+{
+    public bool IsPalindrome(string s)
+    {
+        var left = 0;
+        var right = s.Length - 1;
+        while (left <= right)
+        {
+            var leftCharacter = s[left];
+            var rightChacter = s[right];
+            if (!char.IsLetterOrDigit(leftCharacter))
+            {
+                left++;
+                continue;
+            }
+            if (!char.IsLetterOrDigit(rightChacter))
+            {
+                right--;
+                continue;
+            }
+
+            if (char.ToLower(leftCharacter) != char.ToLower(rightChacter))
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
--- Project/Program.cs
+++ Project/Program.cs
@@ -18,4 +18,9 @@ static void TwoPointers()
     var TwoSum_Output = new TwoSum().Solution(input, target);
     Console.WriteLine($"Two sum indexes: {TwoSum_Output[0]}, {TwoSum_Output[1]}");
     #endregion TwoSum
+
+    #region IsPalindrome
+    var IsPalindrome_Output = new Palindrome().IsPalindrome("A man, a plan, a canal: Panama");
+    Console.WriteLine($"IsPalindrome: {IsPalindrome_Output}");
+    #endregion
 }
