1bfe976f44ff58adbd07b6abe0b08699d6777d51
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-11-24 19:44:56 +0330

    Add 1071-GreatestCommonDivisorOfStrings

---
+++ Project/Problems/SlidingWindow/1071-GreatestCommonDivisorOfStrings.cs
@@ -0,0 +1,26 @@
+﻿namespace Project.Problems.SlidingWindow;
+
+public static class GreatestCommonDivisorOfStrings
+{
+    public static string GcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1)//Equal: When both strings have been created on a basic string.
+        {
+            return "";
+        }
+
+        var gcd = FindGreatestCommonDivisor(str1.Length, str2.Length);//Based on Math (Find Greatest Common Divisor
+        return str1[..gcd];
+    }
+
+    private static int FindGreatestCommonDivisor(int a, int b)
+    {
+        while (b != 0)
+        {
+            var temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
