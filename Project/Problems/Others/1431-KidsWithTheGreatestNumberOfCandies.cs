c3ca9613c37b44748b564fb5da76f0a196ad24e3
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-11-25 23:01:37 +0330

    Add 1431-KidsWithTheGreatestNumberOfCandies

---
+++ Project/Problems/Others/1431-KidsWithTheGreatestNumberOfCandies.cs
@@ -0,0 +1,24 @@
+﻿namespace Project.Problems.Others;
+
+public static class KidsWithTheGreatestNumberOfCandies
+{
+    public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        var result = new bool[candies.Length];
+
+        var max = 0;
+        for (var i = 0; i < candies.Length; i++)
+        {
+            if (candies[i] > max)
+            {
+                max = candies[i];
+            }
+        }
+        for (var i = 0; i < candies.Length; i++)
+        {
+            result[i] = candies[i] + extraCandies >= max;
+        }
+
+        return result;
+    }
+}
