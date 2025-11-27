ee01c18c47c1b45e669dbde1eb6a4612b4696b11
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-11-27 12:30:51 +0330

    Add CanPlaceFlowers

---
+++ Project/Problems/Others/605-CanPlaceFlowers.cs
@@ -0,0 +1,25 @@
+﻿namespace Project.Problems.Others;
+
+public static class CanPlaceFlowers
+{
+    public static bool CanPlace(int[] flowerbed, int n)
+    {
+        for (int i = 0; i < flowerbed.Length && n > 0; i++)
+        {
+            if (flowerbed[i] == 0)
+            {
+                var emptyLeft = i == 0 || flowerbed[i - 1] == 0;
+                var emptyRight = i == flowerbed.Length - 1 || flowerbed[i + 1] == 0;
+
+                if (emptyLeft && emptyRight)
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+
+        }
+
+        return n == 0;
+    }
+}
