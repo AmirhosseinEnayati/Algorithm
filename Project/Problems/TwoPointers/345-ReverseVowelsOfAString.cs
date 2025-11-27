77b1d249963df57246346489f525b5015bd7e8ef
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-11-27 13:57:10 +0330

    Add 345-ReverseVowelsOfAString

---
+++ Project/Problems/TwoPointers/345-ReverseVowelsOfAString.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Project.Problems.TwoPointers;
+
+public class ReverseVowelsOfAString
+{
+    private readonly HashSet<char> vowelLetters = new HashSet<char>()
+    {
+        'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'
+    };
+    public string ReverseVowels(string s)
+    {
+        var stringBuilder = new StringBuilder(s);
+        int i = 0, j = s.Length - 1;
+
+        while (i < j)
+        {
+            var isVowelFromFirst = IsVowel(stringBuilder[i]);
+            var isVowelFromLast = IsVowel(stringBuilder[j]);
+            if (!isVowelFromFirst)
+            {
+                i++;
+            }
+
+            if (!isVowelFromLast)
+            {
+                j--;
+            }
+
+            if (!isVowelFromFirst || !isVowelFromLast)
+                continue;
+
+            (stringBuilder[j], stringBuilder[i]) = (stringBuilder[i], stringBuilder[j]);
+            i++;
+            j--;
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    private bool IsVowel(char character)
+    {
+        return vowelLetters.Contains(character);
+    }
+}
