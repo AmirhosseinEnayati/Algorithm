c9276f0687d5c6bb7abc0246197aaed7d396846a
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-11-30 17:37:16 +0330

    Add 151-ReverseWordsInAString

---
+++ Project/Problems/Others/151-ReverseWordsInAString.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Project.Problems.Others;
+
+public static class ReverseWordsInAString
+{
+    public static string ReverseWords(string s)
+    {
+        var letterStack = new Stack<string>();
+        var word = new StringBuilder();
+        for (int i = 0; i < s.Length; i++)
+        {
+            var letter = s[i];
+            if (letter == ' ')//Is it a new word?
+            {
+                if (word.Length == 0)
+                    continue;
+                letterStack.Push(word.ToString());
+                word.Clear();
+            }
+            else
+            {
+                word.Append(letter);
+
+                if (i == s.Length - 1)
+                {
+                    letterStack.Push(word.ToString());
+                    word.Clear();
+                }
+            }
+        }
+
+        while (true)
+        {
+            word.Append(letterStack.Pop());
+            if (letterStack.Count > 0)
+            {
+                word.Append(" ");
+            }
+            else
+                break;
+        }
+        return word.ToString();
+    }
+}
