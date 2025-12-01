0350a2783d68bacb2ea413176f1cc6fea8249e5f
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-01 19:18:29 +0330

    Add 334-IncreasingTripletSubsequence

---
+++ Project/Problems/Others/334-IncreasingTripletSubsequence.cs
@@ -0,0 +1,26 @@
+﻿namespace Project.Problems.Others;
+
+public class IncreasingTripletSubsequence
+{
+    public bool IncreasingTriplet(int[] nums)
+    {
+        if (nums.Length < 3)
+            return false;
+
+        var first = int.MaxValue;
+        var mid = int.MaxValue;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+            if (num > mid)
+                return true;
+            if (first >= num)
+                first = num;
+            else
+                mid = num;
+        }
+
+        return false;
+    }
+}
