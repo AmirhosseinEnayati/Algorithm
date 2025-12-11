46ee065693e8b75d85868f7167997171fb525eb6
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-11 16:14:25 +0330

    Add 283-MoveZeroes

---
+++ Project/Problems/TwoPointers/283-MoveZeroes.cs
@@ -0,0 +1,28 @@
+﻿namespace Project.Problems.TwoPointers;
+
+public class MoveZeroes
+{
+    public void Solution(int[] nums)
+    {
+        if (nums.Length == 1)
+            return;
+
+        int l = 0, r = 1;
+        while (l < r && r < nums.Length)
+        {
+            if (nums[l] == 0 && nums[r] != 0)//Switch only when one of them is zero and the other is non-zero.
+            {
+                (nums[l], nums[r]) = (nums[r], nums[l]);
+                l++;
+                r++;
+
+                continue;
+            }
+
+            if (nums[l] != 0)//Remain at the current position, it may change in the next iteration.
+                l++;
+
+            r++;
+        }
+    }
+}
