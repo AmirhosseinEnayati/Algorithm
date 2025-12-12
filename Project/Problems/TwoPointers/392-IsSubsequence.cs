c6683d9c306d1ced0e693ee4e83a40b3f2f90007
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-12 13:34:21 +0330

    Add 392-IsSubsequence

---
+++ Project/Problems/TwoPointers/392-IsSubsequence.cs
@@ -0,0 +1,32 @@
+﻿namespace Project.Problems.TwoPointers;
+
+public class IsSubsequence
+{
+    public bool Solution(string s, string t)
+    {
+        if (s.Length == 0)
+            return true;
+
+        if (t.Length == 0)
+            return false;
+
+        int i = 0, j = 0;
+
+        while (j < t.Length)
+        {
+            if (t[j] == s[i])
+            {
+                i++;
+            }
+
+            if (i == s.Length)
+            {
+                return true;
+            }
+
+            j++;
+        }
+
+        return false;
+    }
+}
