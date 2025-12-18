4df2c66e20f8a722f4d407617ed9648acfe3e074
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-18 11:56:46 +0330

    Add 1679-MaxNumberOfKSumPairs

---
+++ Project/Problems/TwoPointers/1679-MaxNumberOfKSumPairs.cs
@@ -0,0 +1,33 @@
+﻿namespace Project.Problems.TwoPointers;
+
+public class MaxNumberOfKSumPairs
+{
+    public int MaxOperations(int[] nums, int k)
+    {
+        Array.Sort(nums);
+
+        int left = 0, right = nums.Length - 1, count = 0;
+
+        while (left < right)
+        {
+            var sum = nums[left] + nums[right];
+
+            if (sum == k)
+            {
+                count++;
+                left++;
+                right--;
+            }
+            else if (sum < k)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return count;
+    }
+}
