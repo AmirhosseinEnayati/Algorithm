578b2ac25d0fc408d675d8044780668007001a6a
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-29 21:50:56 +0330

    Add 1004-MaxConsecutiveOnes

---
+++ Project/Problems/SlidingWindow/1004-MaxConsecutiveOnes.cs
@@ -0,0 +1,26 @@
+﻿namespace Project.Problems.SlidingWindow;
+
+public class MaxConsecutiveOnes
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        int left = 0, right = 0, max = 0, zeroCount = 0;
+
+        while (right < nums.Length)
+        {
+            if (nums[right] == 0) zeroCount++;
+            while (zeroCount > k)
+            {
+                if (nums[left] == 0) zeroCount--;
+                left++;
+            }
+            var winSize = right - left + 1;
+            if (winSize > max)
+                max = winSize;
+
+            right++;
+        }
+
+        return max;
+    }
+}
