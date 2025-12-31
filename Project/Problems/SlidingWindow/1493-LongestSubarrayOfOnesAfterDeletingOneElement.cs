c024fbc871dfbf7cd61ff73eb1d357a949382b0e
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-31 20:09:42 +0330

    Add 1493-LongestSubarrayOfOnesAfterDeletingOneElement

---
+++ Project/Problems/SlidingWindow/1493-LongestSubarrayOfOnesAfterDeletingOneElement.cs
@@ -0,0 +1,27 @@
+﻿namespace Project.Problems.SlidingWindow;
+
+public class LongestSubarrayOfOnesAfterDeletingOneElement
+{
+    public int LongestSubarray(int[] nums)
+    {
+        int left = 0, right = 0, maxSize = 0, firstZeroIndex = -1;
+        while (right < nums.Length)
+        {
+            if (nums[right] == 0)
+            {
+                if (firstZeroIndex != -1)
+                    left = firstZeroIndex + 1;
+
+                firstZeroIndex = right;
+            }
+
+            var winSize = right - left + 1;
+            if (winSize > maxSize)
+                maxSize = winSize;
+
+            right++;
+        }
+
+        return maxSize - 1;//-1: for removing one single element
+    }
+}
