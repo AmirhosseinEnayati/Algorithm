95fdde62718ec63b1759de51128adefa8a20bd04
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-01-08 17:33:14 +0330

    Add 1207-UniqueNumberOfOccurrences

---
+++ Project/Problems/Others/1207-UniqueNumberOfOccurrences.cs
@@ -0,0 +1,31 @@
+﻿namespace Project.Problems.Others;
+
+public class UniqueNumberOfOccurrences
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        var numbersFrequncy = new Dictionary<int, int>();
+        var seenNumbersFrequency = new HashSet<int>();
+
+        foreach (var number in arr)
+        {
+            if(numbersFrequncy.ContainsKey(number))
+            {
+                numbersFrequncy[number]++;
+            }
+            else
+            {
+                numbersFrequncy.Add(number, 1);
+            }
+        }
+
+        foreach (var frequency in numbersFrequncy.Values)
+        {
+            if (seenNumbersFrequency.Contains(frequency))
+                return false;
+            seenNumbersFrequency.Add(frequency);
+        }
+
+        return true;
+    }
+}
