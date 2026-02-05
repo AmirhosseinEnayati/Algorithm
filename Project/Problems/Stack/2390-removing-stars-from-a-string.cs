e7314c1de9372a1399be7e07a579aeeeeea59c75
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-02-05 12:52:33 +0330

    Add 2390-Removing_stars_from_a_string

---
+++ Project/Problems/Stack/2390-removing-stars-from-a-string.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace Project.Problems.Stack;
+
+public class Removing_stars_from_a_string
+{
+    public string RemoveStars(string s)
+    {
+        /*var stack = new Stack<char>();
+
+        foreach (var letter in s)
+        {
+            if (letter == '*')
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(letter);
+            }
+        }
+
+
+        var array = stack.ToArray();
+        Array.Reverse(array);
+
+        return new string(array);*/
+
+
+        var stringBuilder = new StringBuilder(s.Length);
+
+        foreach (var letter in s)
+        {
+            if (letter == '*')
+            {
+                stringBuilder.Length--;
+            }
+            else
+            {
+                stringBuilder.Append(letter);
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+}
