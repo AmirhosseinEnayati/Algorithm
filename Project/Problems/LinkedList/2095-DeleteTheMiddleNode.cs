52fe1a84e428c3ed1d64866766299531bb2e19bd
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-04-26 21:18:49 +0330

    Change 2095-DeleteTheMiddleNode

--- Project/Problems/LinkedList/2095-DeleteTheMiddleNode.cs
+++ Project/Problems/LinkedList/2095-DeleteTheMiddleNode.cs
@@ -48,14 +48,3 @@ public class DeleteTheMiddleNode
         return head;
     }*/
 }
-
-public class ListNode
-{
-    public int val;
-    public ListNode next;
-    public ListNode(int val = 0, ListNode next = null)
-    {
-        this.val = val;
-        this.next = next;
-    }
-}
