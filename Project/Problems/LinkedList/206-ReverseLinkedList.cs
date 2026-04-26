3dcd6e9c3daf2d030a8dbdc4df57144af050d5cb
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-04-26 21:18:17 +0330

    Add 206-ReverseLinkedList

---
+++ Project/Problems/LinkedList/206-ReverseLinkedList.cs
@@ -0,0 +1,17 @@
+﻿namespace Project.Problems.LinkedList;
+
+public class ReverseLinkedList
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        var currHead = head;
+        while (head.next is not null)
+        {
+            var prevHead = currHead;
+            currHead = head.next;
+            head.next = currHead.next;
+            currHead.next = prevHead;
+        }
+        return currHead;
+    }
+}
