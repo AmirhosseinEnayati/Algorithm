1e79822cdc367ca5e466a355eae827b5378b4be9
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-04-26 21:16:00 +0330

    Add 394-DecodeString

---
+++ Project/Problems/Stack/394-DecodeString.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Project.Problems.Stack;
+
+public class DecodeString2
+{
+    public string Solution(string s)
+    {
+        int i = 0;
+        StringBuilder str = new();
+
+        while (i < s.Length && s[i] != ']')
+        {
+            if (!char.IsDigit(s[i]))
+            {
+                str.Append(s[i]);
+            }
+            else
+            {
+                int num = 0;
+
+                while (i < s.Length && char.IsDigit(s[i]))
+                {
+                    num = num * 10 + s[i] - '0';
+                    i++;
+                }
+                i++;
+
+                var st = Solution(s);
+
+                while (num > 0)
+                {
+                    num--;
+                    str.Append(st);
+                }
+            }
+            i++;
+        }
+
+        return str.ToString();
+    }
+}
