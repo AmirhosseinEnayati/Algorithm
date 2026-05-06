9455d5510dfdd8d659042a931249869d5c8babde
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-05-06 20:08:02 +0330

    Add 1448 (CountGoodNodesInBinaryTree)

---
+++ Project/Problems/BinaryTree/1448-CountGoodNodesInBinaryTree.cs
@@ -0,0 +1,24 @@
+﻿namespace Project.Problems.BinaryTree;
+
+public class CountGoodNodesInBinaryTree
+{
+    int max = 0, goodNodesCount = 0;
+    public int GoodNodes(TreeNode root)
+    {
+        if (root.val >= max)
+        {
+            max = root.val;
+            goodNodesCount++;
+        }
+        if (root.left is not null)
+        {
+            GoodNodes(root.left);
+        }
+        if (root.right is not null)
+        {
+            GoodNodes(root.right);
+        }
+
+        return goodNodesCount;
+    }
+}
