9571bf4f8cc11b04a40e12a63aa4903640100f7d
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-05-07 18:43:06 +0330

    Add 437

---
+++ Project/Problems/BinaryTree/437-PathSumIII.cs
@@ -0,0 +1,31 @@
+﻿namespace Project.Problems.BinaryTree;
+
+public class PathSumIII
+{
+    Dictionary<long,int> seenPrefixSums = new Dictionary<long, int>() { { 0, 1 } };
+    public int Solution(TreeNode root, int targetSum)
+    {
+        var output = 0; 
+
+        DFS(root, 0);
+
+        return output;
+
+        void DFS(TreeNode node, long prefixSum)
+        {
+            if (node == null)
+                return;
+
+            prefixSum += node.val;
+            output += GetPreviousPrefixSum(targetSum, prefixSum);
+            seenPrefixSums[prefixSum] = seenPrefixSums.GetValueOrDefault(prefixSum, 0) + 1;
+
+            DFS(node.left, prefixSum);
+            DFS(node.right, prefixSum);
+
+            seenPrefixSums[prefixSum]--;
+        }
+    }
+
+    private int GetPreviousPrefixSum(int targetSum, long prefixSum) => seenPrefixSums.GetValueOrDefault(prefixSum - targetSum, 0);
+}
