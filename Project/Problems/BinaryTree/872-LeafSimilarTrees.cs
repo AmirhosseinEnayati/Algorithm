9565410725cfca4feaf58770e2a168cc01e2b7ab
AmirhosseinEnayati <enayati.amirh@gmail.com>
2026-05-08 18:58:51 +0330

    Add 872

--- Project/Problems/BinaryTree/872-LeafSimilarTrees.cs
+++ Project/Problems/BinaryTree/872-LeafSimilarTrees.cs
@@ -2,27 +2,50 @@ namespace Project.Problems.BinaryTree;
 
 public class LeafSimilarTrees
 {
+    private int leaves2Count = 0;
+    private List<int> leaves1;
     public bool LeafSimilar(TreeNode root1, TreeNode root2)
     {
-        var leaves1 = new List<int>();
-        var leaves2 = new List<int>();
+        leaves1 = new List<int>();
+        CollectLeaves(root1, leaves1);
 
-        GetLeavesDFS(root1, leaves1);
-        GetLeavesDFS(root2, leaves2);
+        var isSimilar = CompareWithLeaves1(root2);
 
-        return leaves1.SequenceEqual(leaves2);
+        // اگر برگ‌های درخت دوم تمام شد ولی درخت اول هنوز برگ دارد → false
+        return isSimilar && leaves2Count == leaves1.Count;
     }
 
-    private void GetLeavesDFS(TreeNode? node, List<int> leaves)
+
+    private void CollectLeaves(TreeNode? node, List<int> leaves)
     {
         if (node is null) return;
 
         if (node.left is null && node.right is null)
         {
             leaves.Add(node.val);
+            return;
         }
 
-        GetLeavesDFS(node.left, leaves);
-        GetLeavesDFS(node.right, leaves);
+        CollectLeaves(node.left, leaves);
+        CollectLeaves(node.right, leaves);
+    }
+
+    private bool CompareWithLeaves1(TreeNode? node)
+    {
+        if (node is null) return true;
+
+        if (node.left is null && node.right is null)
+        {
+            if (leaves2Count >= leaves1.Count)
+                return false;
+
+            if (node.val != leaves1[leaves2Count])
+                return false;
+
+            leaves2Count++;
+            return true;
+        }
+
+        return CompareWithLeaves1(node.left) && CompareWithLeaves1(node.right);
     }
 }
