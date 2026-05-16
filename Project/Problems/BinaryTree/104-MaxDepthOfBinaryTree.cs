322afcd78b3f3499f3729e2b5b03842c67880cf1
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-05-16 22:30:30 +0330

    Add 104 (MaxDepthOfBinaryTree)

---
+++ Project/Problems/BinaryTree/104-MaxDepthOfBinaryTree.cs
@@ -0,0 +1,12 @@
+﻿namespace Project.Problems.BinaryTree;
+
+public class MaxDepthOfBinaryTree
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null) { return 0; }
+        var left = MaxDepth(root.left);
+        var right = MaxDepth(root.right);
+        return (left > right ? left : right) + 1;
+    }
+}
