cc9866e3f33eda6a8a61a41f59b5befe94c49c49
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-05-21 17:18:04 +0330

    Add 199 (BinaryTreeRightSideView)

---
+++ Project/Problems/BinaryTree/199-BinaryTreeRightSideView.cs
@@ -0,0 +1,22 @@
+﻿namespace Project.Problems.BinaryTree;
+
+public class BinaryTreeRightSideView
+{
+    private List<int> result = new List<int>();
+    public IList<int> RightSideView(TreeNode root)
+    {
+        DFS(root, 0);
+        return result;
+    }
+
+    private void DFS(TreeNode node, int depth)
+    {
+        if (node == null) return;
+
+        if (depth == result.Count)
+            result.Add(node.val);
+
+        DFS(node.right, depth + 1);
+        DFS(node.left, depth + 1);
+    }
+}
