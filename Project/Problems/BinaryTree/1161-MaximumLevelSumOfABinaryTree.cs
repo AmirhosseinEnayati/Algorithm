831cde8bd356513391fa428ef2b939928f4e39ca
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-05-22 13:42:45 +0330

    Add 1161 (MaximumLevelSumOfABinaryTree)

---
+++ Project/Problems/BinaryTree/1161-MaximumLevelSumOfABinaryTree.cs
@@ -0,0 +1,36 @@
+﻿namespace Project.Problems.BinaryTree;
+
+public class MaximumLevelSumOfABinaryTree
+{
+    private List<int> result = new List<int>();
+    public int MaxLevelSum(TreeNode root)
+    {
+        DFS(root, 0);
+
+        int maxSum = int.MinValue, maxLevel = 0;//maxSum inital value should not be zero because all of the nodes' value can be negative.
+        for (int i = 0; i < result.Count; i++)
+        {
+            if (result[i] > maxSum)
+            {
+                maxSum = result[i];
+                maxLevel = i;
+            }
+        }
+
+        return maxLevel + 1;
+    }
+
+    private void DFS(TreeNode node, int level)
+    {
+        if (node is null)
+            return;
+
+        if (level == result.Count)
+            result.Add(node.val);
+        else
+            result[level] += node.val;
+
+        DFS(node.left, level + 1);
+        DFS(node.right, level + 1);
+    }
+}
