c185cf1c9c367f97e52d3ca196e6cff43ef587e9
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-06-08 21:45:29 +0330

    Add 841 (KeysAndRooms)

---
+++ Project/Problems/BinaryTree/841-KeysAndRooms.cs
@@ -0,0 +1,24 @@
+﻿namespace Project.Problems.BinaryTree;
+
+public class KeysAndRooms
+{
+    public bool CanVisitAllRooms(IList<IList<int>> rooms)
+    {
+        HashSet<int> visited = [0];
+        DFS(0);
+
+        return rooms.Count == visited.Count;
+
+        void DFS(int room)
+        {
+            foreach (var keys in rooms[room])
+            {
+                if (visited.Contains(keys))
+                    continue;
+
+                visited.Add(keys);
+                DFS(keys);
+            }
+        }
+    }
+}
