9b046309abf00c689bca71872663eda6a2846ae5
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-06-12 18:51:57 +0330

    Add 547 (NumberOfProvinces)

---
+++ Project/Problems/BinaryTree/547-NumberOfProvinces.cs
@@ -0,0 +1,31 @@
+﻿namespace Project.Problems.BinaryTree;
+
+public class NumberOfProvinces
+{
+    public int FindCircleNum(int[][] isConnected)
+    {
+        var provinceCount = 0;
+        var visitedNodes = new bool[isConnected.Length];
+
+        for (var i = 0; i < isConnected.Length; ++i)
+        {
+            if (!visitedNodes[i])
+            {
+                DFS(i);
+                provinceCount++;
+            }
+        }
+        return provinceCount;
+
+
+        void DFS(int node)
+        {
+            visitedNodes[node] = true;
+            for (var i = 0; i < isConnected.Length; ++i)
+            {
+                if (isConnected[node][i] == 1 && !visitedNodes[i])
+                    DFS(i);
+            }
+        }
+    }
+}
