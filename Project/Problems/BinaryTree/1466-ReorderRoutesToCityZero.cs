e02e3b6ad74df7a04408dc611ba18f48f859b131
Amirhossein Enayati <enayati.amirh@gmail.com>
2026-06-24 22:16:07 +0330

    Add 1466 (ReorderRoutesToCityZero)

---
+++ Project/Problems/BinaryTree/1466-ReorderRoutesToCityZero.cs
@@ -0,0 +1,45 @@
+﻿namespace Project.Problems.BinaryTree;
+
+public static class ReorderRoutesToCityZero
+{
+    public static int MinReorder(int n, int[][] connections)
+    {
+        var count = 0;
+        var graph = CreateGraph(n, connections);
+        Dfs(0, -1);
+        return count;
+
+
+        void Dfs(int node, int parent)
+        {
+            foreach (var edge in graph[node])
+            {
+                if (edge.OtherSideNode != parent)
+                {
+                    count += edge.Sign;
+                    Dfs(edge.OtherSideNode, node);
+                }
+            }
+        }
+    }
+
+    private static List<Edge>[] CreateGraph(int n, int[][] connections)
+    {
+        var graph = new List<Edge>[n];
+        for (int i = 0; i < n; i++)//initialize graph
+        {
+            graph[i] = [];
+        }
+        foreach (var connection in connections)//set graph values
+        {
+            int from = connection[0];
+            int to = connection[1];
+
+            graph[from].Add(new Edge(to, 1));
+            graph[to].Add(new Edge(from, 0));
+        }
+
+        return graph;
+    }
+    record Edge(int OtherSideNode, int Sign);
+}
